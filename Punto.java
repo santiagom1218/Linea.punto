02f35b02390b3488f959a38386463456ad1dd487
santiagom1218 <65306624+santiagom1218@users.noreply.github.com>
2020-08-21 09:40:55 -0500

    Update Punto.java

--- Punto.java
+++ Punto.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class Punto{
   private int x;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class Punto{
+  private int x;
   private int y;
-  
-  public Punto(int x, int y, int z) {
+
+  public Punto(int x, int y) {
       this.x = x;
       this.y = y;
-      this.z = z;
-      
-  } 
-
+  }
+  
   public void setx(int x){
     this.x=x;
   }
@@ -20,38 +22,62 @@ public class Punto{
   }
   Scanner s= new Scanner(System.in);
   int z;
-
+  float res;
 
   public void leer(){
-    Scanner s= new Scanner(System.in);
-    x=s.nextInt();
-    y=s.nextInt();
+    
+    Scanner s=new Scanner(System.in);
+    try
+    {
+      x=s.nextInt();
+      y=s.nextInt();
+    }
+    catch(InputMismatchException ex)
+    {
+      System.out.println("\n(No es un numero, por defecto se le asigna 0");
+      this.x=0;
+      this.y=0;
+    }
   }
   public void imprimir(){
-    System.out.println("("+x+","+y+")");
+    System.out.print("("+x+","+y+")");
   }
   public void distancia(Punto a,Punto b){
     res=(float)Math.sqrt(Math.pow(a.x-b.x,2)+Math.pow(a.y-b.y,2));
-    System.out.println(distancia);
+    System.out.print(res);
   }
   public void origen(){
     res=(float)Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2));
-    System.out.println("distacia origen: "+distancia);
-    System.out.println("");
+    System.out.print("/distacia origen: "+res);
   }
   public void horizontal(){
-    System.out.println("*horizontal: ");
-    this.x+=s.nextInt();
+    System.out.print("moviento horizontal: ");
+    try{
+      this.x+=s.nextInt();
+    }catch(InputMismatchException ex){
+      System.out.println("\n(No es un numero, por defecto se le asigna 0");
+      this.x+=0;
+    }
+    
   }
   public void vertical(){
-    System.out.println("*vertical: ");
-    this.y+=s.nextInt();
+    System.out.print("moviento vertical: ");
+    try{
+      this.x+=s.nextInt();
+    }catch(InputMismatchException ex){
+     System.out.println("\n(No es un numero, por defecto se le asigna 0");
+      this.y+=0;
+    }
   }
   public void diagonal(Punto b){
-    System.out.println("*diagonal: ");
-    z=s.nextInt();
+    System.out.print("moviento diagonal: ");
+     try{
+      z=s.nextInt();
+    }catch(InputMismatchException ex){
+      System.out.println("\n(No es un numero, por defecto se le asigna 0");
+      this.z=0;
+    }
     this.x+=z;
     this.y+=z;
-  }
-  
+  } 
 }
